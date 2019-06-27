61d73faf325044b6620bd6966d877f7ed29297fc
call900913 <call900913@icloud.com>
2019-06-27 11:27:05 -0400

    Replace var with let

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let restaurants,
   neighborhoods,
-  cuisines
-var newMap
-var markers = []
+  cuisines,
+  newMap
+let markers = []
 
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
